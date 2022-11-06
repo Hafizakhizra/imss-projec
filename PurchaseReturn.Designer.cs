24971a61441d32e629244459cc6acf4815f81f22
Hafizakhizra <91067375+Hafizakhizra@users.noreply.github.com>
2022-11-06 20:31:22 +0500

    Add files via upload

---
+++ PurchaseReturn.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace imss
+{
+    partial class PurchaseReturn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.purchaseIDTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.quanTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.quanErrorLabel = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.suppErrorLabel = new System.Windows.Forms.Label();
+            this.supplierTxt = new System.Windows.Forms.TextBox();
+            this.userTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.purchaseIDGV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.barcodeGV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.proIDGV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productGV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantityGV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.perUnitPriceGV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.totalAmountGV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.leftPanel.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.rightPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backBtn
+            // 
+            this.backBtn.FlatAppearance.BorderSize = 0;
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.Controls.Add(this.userTxt);
+            this.leftPanel.Controls.Add(this.label4);
+            this.leftPanel.Controls.Add(this.label7);
+            this.leftPanel.Controls.Add(this.supplierTxt);
+            this.leftPanel.Controls.Add(this.label5);
+            this.leftPanel.Controls.Add(this.quanTxt);
+            this.leftPanel.Controls.Add(this.label6);
+            this.leftPanel.Controls.Add(this.quanErrorLabel);
+            this.leftPanel.Controls.Add(this.label3);
+            this.leftPanel.Controls.Add(this.dateTimePicker1);
+            this.leftPanel.Controls.Add(this.loadBtn);
+            this.leftPanel.Controls.Add(this.purchaseIDTxt);
+            this.leftPanel.Controls.Add(this.label2);
+            this.leftPanel.Controls.Add(this.suppErrorLabel);
+            this.leftPanel.Controls.SetChildIndex(this.suppErrorLabel, 0);
+            this.leftPanel.Controls.SetChildIndex(this.panel2, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label2, 0);
+            this.leftPanel.Controls.SetChildIndex(this.purchaseIDTxt, 0);
+            this.leftPanel.Controls.SetChildIndex(this.loadBtn, 0);
+            this.leftPanel.Controls.SetChildIndex(this.dateTimePicker1, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label3, 0);
+            this.leftPanel.Controls.SetChildIndex(this.quanErrorLabel, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label6, 0);
+            this.leftPanel.Controls.SetChildIndex(this.quanTxt, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label5, 0);
+            this.leftPanel.Controls.SetChildIndex(this.supplierTxt, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label7, 0);
+            this.leftPanel.Controls.SetChildIndex(this.label4, 0);
+            this.leftPanel.Controls.SetChildIndex(this.userTxt, 0);
+            // 
+            // rightPanel
+            // 
+            this.rightPanel.Controls.Add(this.dataGridView1);
+            this.rightPanel.Controls.SetChildIndex(this.panel3, 0);
+            this.rightPanel.Controls.SetChildIndex(this.dataGridView1, 0);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd-MMM-yyyy hh:mm:ss tt";
+            this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(10, 237);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(222, 23);
+            this.dateTimePicker1.TabIndex = 25;
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.BackColor = System.Drawing.Color.IndianRed;
+            this.loadBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.loadBtn.FlatAppearance.BorderSize = 2;
+            this.loadBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loadBtn.ForeColor = System.Drawing.Color.White;
+            this.loadBtn.Location = new System.Drawing.Point(8, 180);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(223, 36);
+            this.loadBtn.TabIndex = 24;
+            this.loadBtn.Text = "&LOAD";
+            this.loadBtn.UseVisualStyleBackColor = false;
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // purchaseIDTxt
+            // 
+            this.purchaseIDTxt.Location = new System.Drawing.Point(8, 147);
+            this.purchaseIDTxt.Name = "purchaseIDTxt";
+            this.purchaseIDTxt.Size = new System.Drawing.Size(223, 23);
+            this.purchaseIDTxt.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 15);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Enter PurchaseID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Date";
+            // 
+            // quanTxt
+            // 
+            this.quanTxt.Location = new System.Drawing.Point(8, 325);
+            this.quanTxt.Name = "quanTxt";
+            this.quanTxt.Size = new System.Drawing.Size(221, 23);
+            this.quanTxt.TabIndex = 29;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 15);
+            this.label6.TabIndex = 30;
+            this.label6.Text = "Quantity";
+            // 
+            // quanErrorLabel
+            // 
+            this.quanErrorLabel.AutoSize = true;
+            this.quanErrorLabel.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quanErrorLabel.ForeColor = System.Drawing.Color.Salmon;
+            this.quanErrorLabel.Location = new System.Drawing.Point(214, 305);
+            this.quanErrorLabel.Name = "quanErrorLabel";
+            this.quanErrorLabel.Size = new System.Drawing.Size(20, 28);
+            this.quanErrorLabel.TabIndex = 31;
+            this.quanErrorLabel.Text = "*";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.purchaseIDGV,
+            this.barcodeGV,
+            this.proIDGV,
+            this.productGV,
+            this.quantityGV,
+            this.perUnitPriceGV,
+            this.totalAmountGV});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Hand;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 137);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(826, 570);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 15);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Supplier";
+            // 
+            // suppErrorLabel
+            // 
+            this.suppErrorLabel.AutoSize = true;
+            this.suppErrorLabel.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.suppErrorLabel.ForeColor = System.Drawing.Color.Salmon;
+            this.suppErrorLabel.Location = new System.Drawing.Point(109, 124);
+            this.suppErrorLabel.Name = "suppErrorLabel";
+            this.suppErrorLabel.Size = new System.Drawing.Size(20, 28);
+            this.suppErrorLabel.TabIndex = 34;
+            this.suppErrorLabel.Text = "*";
+            // 
+            // supplierTxt
+            // 
+            this.supplierTxt.Location = new System.Drawing.Point(10, 281);
+            this.supplierTxt.Name = "supplierTxt";
+            this.supplierTxt.Size = new System.Drawing.Size(221, 23);
+            this.supplierTxt.TabIndex = 35;
+            // 
+            // userTxt
+            // 
+            this.userTxt.Location = new System.Drawing.Point(10, 373);
+            this.userTxt.Name = "userTxt";
+            this.userTxt.Size = new System.Drawing.Size(221, 23);
+            this.userTxt.TabIndex = 36;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 355);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 15);
+            this.label4.TabIndex = 37;
+            this.label4.Text = "User";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Salmon;
+            this.label7.Location = new System.Drawing.Point(216, 353);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 28);
+            this.label7.TabIndex = 38;
+            this.label7.Text = "*";
+            // 
+            // purchaseIDGV
+            // 
+            this.purchaseIDGV.HeaderText = "Purchase ID";
+            this.purchaseIDGV.Name = "purchaseIDGV";
+            this.purchaseIDGV.ReadOnly = true;
+            // 
+            // barcodeGV
+            // 
+            this.barcodeGV.HeaderText = "Barcode";
+            this.barcodeGV.Name = "barcodeGV";
+            this.barcodeGV.ReadOnly = true;
+            // 
+            // proIDGV
+            // 
+            this.proIDGV.HeaderText = "proID";
+            this.proIDGV.Name = "proIDGV";
+            this.proIDGV.ReadOnly = true;
+            // 
+            // productGV
+            // 
+            this.productGV.HeaderText = "Product";
+            this.productGV.Name = "productGV";
+            this.productGV.ReadOnly = true;
+            // 
+            // quantityGV
+            // 
+            this.quantityGV.HeaderText = "Quantity";
+            this.quantityGV.Name = "quantityGV";
+            this.quantityGV.ReadOnly = true;
+            // 
+            // perUnitPriceGV
+            // 
+            this.perUnitPriceGV.HeaderText = "Per Unit Price";
+            this.perUnitPriceGV.Name = "perUnitPriceGV";
+            this.perUnitPriceGV.ReadOnly = true;
+            // 
+            // totalAmountGV
+            // 
+            this.totalAmountGV.HeaderText = "Total Amount";
+            this.totalAmountGV.Name = "totalAmountGV";
+            this.totalAmountGV.ReadOnly = true;
+            // 
+            // PurchaseReturn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 770);
+            this.Name = "PurchaseReturn";
+            this.Text = "PurchaseReturn";
+            this.leftPanel.ResumeLayout(false);
+            this.leftPanel.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.rightPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button loadBtn;
+        private System.Windows.Forms.TextBox purchaseIDTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox quanTxt;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label quanErrorLabel;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label suppErrorLabel;
+        private System.Windows.Forms.TextBox userTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox supplierTxt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn purchaseIDGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn barcodeGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn proIDGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantityGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn perUnitPriceGV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn totalAmountGV;
+    }
+}
